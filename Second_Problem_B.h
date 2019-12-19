853713b371ebe8c70af862674222e680da6ee47c
BenSeddikMehdi <amigoinpt@gmail.com>
2019-12-19 19:00:03 +0100

    CNC_2014

--- Second_Problem_B.h
+++ Second_Problem_B.h
@@ -7,28 +7,28 @@
 
 #include <string.h>
 
-typedef struct fileAttente{
+typedef struct queue{
     char fileName[80];
-    struct fileAttente *next;
-}fileAttente_t;
+    struct queue *next;
+}queue_t;
 
-fileAttente_t teteListe_0 = {"File_0.txt,File_1.txt,File_2.txt,File_3.txt",NULL};
-fileAttente teteListe_1 = {"File_4.txt,File_5.txt",NULL};
-fileAttente teteListe_2 = {"File_6.txt",NULL};
-fileAttente teteListe_3 = {"File_7.txt",NULL};
+queue_t pList_0 = {"File_0.txt,File_1.txt,File_2.txt,File_3.txt",NULL};
+queue_t pList_1 = {"File_4.txt,File_5.txt",NULL};
+queue_t pList_2 = {"File_6.txt",NULL};
+queue_t pList_3 = {"File_7.txt",NULL};
 
-fileAttente *teteListe, *teteListe2;
+queue_t *pList, *pList2;
 /* Question 8 */
 /**************/
 int display(){
-    teteListe = &teteListe_0;
-    teteListe_0.next = &teteListe_1;
-    teteListe_1.next = &teteListe_2;
-    teteListe_2.next = &teteListe_3;
+    pList = &pList_0;
+    pList_0.next = &pList_1;
+    pList_1.next = &pList_2;
+    pList_2.next = &pList_3;
     int i = 0;
-    while (teteListe != NULL){
-        printf("%s\n", teteListe->fileName);
-        teteListe = teteListe->next;
+    while (pList != NULL){
+        printf("%s\n", pList->fileName);
+        pList = pList->next;
         i++;
     }
     return i;
@@ -39,12 +39,12 @@ int display(){
 FILE *pFile_0;
 
 void print(){
-    teteListe = &teteListe_2;
-/*    teteListe_0.next = &teteListe_1;
-    teteListe_1.next = &teteListe_2;*/
-    teteListe_2.next = &teteListe_3;
-    if (teteListe != NULL){
-        pFile_0 = fopen(teteListe->fileName,"r");
+    pList = &pList_2;
+/*    pList_0.next = &pList_1;
+    pList_1.next = &pList_2;*/
+    pList_2.next = &pList_3;
+    if (pList != NULL){
+        pFile_0 = fopen(pList->fileName,"r");
         int c = 0;
         while (c != EOF){
             c = fgetc(pFile_0);
@@ -52,40 +52,40 @@ void print(){
         }
         fclose(pFile_0);
         printf("After Reading\n");
-        teteListe = teteListe->next;
+        pList = pList->next;
     }
 }
 
 /* Question 10 */
 /**************/
 void removeFunc(char fileN[]){
-    teteListe2 = &teteListe_0;
-    teteListe_0.next = &teteListe_1;
-    teteListe_1.next = &teteListe_2;
-    teteListe_2.next = &teteListe_3;
-    if(teteListe2 != NULL) {
-        while (strcmp(teteListe2->fileName, "") == 0) {
-            teteListe2 = teteListe2->next;
-            if (teteListe2 == NULL) {
+    pList2 = &pList_0;
+    pList_0.next = &pList_1;
+    pList_1.next = &pList_2;
+    pList_2.next = &pList_3;
+    if(pList2 != NULL) {
+        while (strcmp(pList2->fileName, "") == 0) {
+            pList2 = pList2->next;
+            if (pList2 == NULL) {
                 strcpy(fileN,"");
                 break;
             }
         }
-        if (teteListe2 != NULL) {
+        if (pList2 != NULL) {
             char fil[80] = "";
             int i = 0, j = 0;
-            for (i = 0; teteListe2->fileName[i] != '\0'; ++i) {
-                if (teteListe2->fileName[i] == ',') {
+            for (i = 0; pList2->fileName[i] != '\0'; ++i) {
+                if (pList2->fileName[i] == ',') {
                     break;
                 }
-                fil[i] = teteListe2->fileName[i];
+                fil[i] = pList2->fileName[i];
             }
             strcpy(fileN, fil);
-            for (j = 0; teteListe2->fileName[i] != '\0'; ++j) {
+            for (j = 0; pList2->fileName[i] != '\0'; ++j) {
                 i++;
-                teteListe2->fileName[j] = teteListe2->fileName[i];
+                pList2->fileName[j] = pList2->fileName[i];
             }
-            teteListe2->fileName[j] = '\0';
+            pList2->fileName[j] = '\0';
         }
     }
 }
